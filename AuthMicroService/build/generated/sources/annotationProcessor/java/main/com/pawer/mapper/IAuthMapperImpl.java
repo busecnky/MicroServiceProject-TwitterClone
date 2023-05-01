b43838a0b104c823cceed472bdb4a2565ba13e27
busecnky <cankayabuse94@gmail.com>
2023-05-01 23:46:14 +0300

    frontend tarafı halloldu.

--- AuthMicroService/build/generated/sources/annotationProcessor/java/main/com/pawer/mapper/IAuthMapperImpl.java
+++ AuthMicroService/build/generated/sources/annotationProcessor/java/main/com/pawer/mapper/IAuthMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-04-26T17:33:46+0300",
+    date = "2023-05-01T13:58:43+0300",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 17.0.6 (Amazon.com Inc.)"
 )
 @Component
